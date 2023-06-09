635c26fdec179b014c4eb2c22941451f4381ed2a
shicheng <wangshicheng@bytedance.com>
2023-06-09 20:55:52 +0800

    feat: init

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,8 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
-// const packageJson = require("./package.json");
+//NEW
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default [
   {
@@ -21,14 +24,22 @@ export default [
       },
     ],
     plugins: [
+      // NEW
+      peerDepsExternal(),
+
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+
+      // NEW
+      terser(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
   },
 ];
